16e1c9a0a827be68292d550ee997d25bcc94245f
Maarten De Schrijver <maartendeschrijver@gmail.com>
2018-07-13 13:16:51 +0200

    Move main() to end of script

--- src/test_complex_objects.py
+++ src/test_complex_objects.py
@@ -16,51 +16,12 @@ BASEURL_PRD = 'https://archief-qas.viaa.be'
 PATH = '/mediahaven-rest-api/resources/media/'
 
 
-def main():
-    arguments = Arguments()
-    init_logger()
-    parser = argparse.ArgumentParser()
-    parser.add_argument('-u', '--username', type=str, dest='username',
-            required=True,
-            help='Username to use when connecting to mediahaven.')
-    parser.add_argument('-p', '--password', type=str, dest='password',
-            required=True,
-            help='Password to use when connecting to mediahaven.')
-    parser.add_argument('-e', '--environment', type=str, choices=['QAS', 'PRD'],
-            help='The environment to test. QAS or PRD.')
-    parser.parse_args(namespace=arguments)
-
-    check_arguments(arguments)
-
-    logging.info('Running in environment: ' + arguments.environment)
-
-    getseturl = arguments.baseurl + PATH
-    # We need to construct the query ourselves (see https://github.com/requests/requests/issues/1454).
-    # If we don't, the URL will be: '/mediahaven-rest-api/resources/media/?q=%2B%28MediaObjectType%3ASet%29&nrOfResults=1'
-    # Request multiple sets since not all will have children
-    getsetrequest = requests.get(getseturl + '?q=%2B(MediaObjectType:Set)',
-                                 auth=HTTPBasicAuth(arguments.username, arguments.password))
-    getsetresponse = getsetrequest.json();
-    nr_of_results = getsetresponse['totalNrOfResults']
-    logging.info('totalNrOfResults is: %s' % nr_of_results)
-    setresults = getsetresponse['mediaDataList']
-    if setresults:
-        for complex in setresults:
-            fragmentid = complex['fragmentId']
-            logging.debug('Checking fragmentId "%s" for children...' % fragmentid)
-            nrofchildren = checknumberofchildren(arguments, fragmentid)
-            if nrofchildren > 0:
-                logging.info('Found a set (%s) with %s children.' % (fragmentid, nrofchildren))
-                performoperations(arguments, fragmentid)
-                break
-
 def checknumberofchildren(arguments, fragmentid):
     url = arguments.baseurl + PATH + fragmentid + '/children'
     getchildrenrequest = requests.get(url, auth=HTTPBasicAuth(arguments.username, arguments.password))
     jsonresult = getchildrenrequest.json()
     totalnrofresults = jsonresult['totalNrOfResults']
     return totalnrofresults
-
 
 def performoperations(arguments, fragmentid):
     url = arguments.baseurl + PATH + fragmentid + '/children'
@@ -110,7 +71,6 @@ def performoperations(arguments, fragmentid):
     children = jsonresult['mediaDataList']
     logging.info('Fragment has ' + str(totalnrofresults) + ' children.')
 
-
 def init_logger():
     # create logger with 'spam_application'
     logger = logging.getLogger('')
@@ -133,7 +93,6 @@ def init_logger():
     except PermissionError:
         logging.error("Permission denied for {}".format(logfile))
 
-
 def check_arguments(arguments):
     if arguments.username is None:
         close("No username specified")
@@ -147,10 +106,47 @@ def check_arguments(arguments):
     else:
         arguments.baseurl = BASEURL_PRD
 
-
 def close(message):
     logging.error(message)
     exit(1)
 
+
+def main():
+    arguments = Arguments()
+    init_logger()
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-u', '--username', type=str, dest='username',
+            required=True,
+            help='Username to use when connecting to mediahaven.')
+    parser.add_argument('-p', '--password', type=str, dest='password',
+            required=True,
+            help='Password to use when connecting to mediahaven.')
+    parser.add_argument('-e', '--environment', type=str, choices=['QAS', 'PRD'],
+            help='The environment to test. QAS or PRD.')
+    parser.parse_args(namespace=arguments)
+
+    check_arguments(arguments)
+
+    logging.info('Running in environment: ' + arguments.environment)
+
+    getseturl = arguments.baseurl + PATH
+    # We need to construct the query ourselves (see https://github.com/requests/requests/issues/1454).
+    # If we don't, the URL will be: '/mediahaven-rest-api/resources/media/?q=%2B%28MediaObjectType%3ASet%29&nrOfResults=1'
+    # Request multiple sets since not all will have children
+    getsetrequest = requests.get(getseturl + '?q=%2B(MediaObjectType:Set)',
+                                 auth=HTTPBasicAuth(arguments.username, arguments.password))
+    getsetresponse = getsetrequest.json();
+    nr_of_results = getsetresponse['totalNrOfResults']
+    logging.info('totalNrOfResults is: %s' % nr_of_results)
+    setresults = getsetresponse['mediaDataList']
+    if setresults:
+        for complex in setresults:
+            fragmentid = complex['fragmentId']
+            logging.debug('Checking fragmentId "%s" for children...' % fragmentid)
+            nrofchildren = checknumberofchildren(arguments, fragmentid)
+            if nrofchildren > 0:
+                logging.info('Found a set (%s) with %s children.' % (fragmentid, nrofchildren))
+                performoperations(arguments, fragmentid)
+                break
 
 main()
