5c466c2feec99f4649a27d113312ceb4060010e4
Maarten De Schrijver <maartendeschrijver@gmail.com>
2018-07-13 14:47:59 +0200

    Refactor argument parser
    - ditch the custom Arguments-object: no need for it
    - ditch check_arguments(): functionality is provided in the standard argument parser
    - add some more logging (and format them properly)

--- src/test_complex_objects.py
+++ src/test_complex_objects.py
@@ -12,19 +12,19 @@ from arguments import Arguments
 __author__ = 'viaa'
 
 BASEURL_QAS = 'https://archief-qas.viaa.be'
-BASEURL_PRD = 'https://archief-qas.viaa.be'
+BASEURL_PRD = 'https://archief.viaa.be'
 PATH = '/mediahaven-rest-api/resources/media/'
 
 
-def checknumberofchildren(arguments, fragmentid):
-    url = arguments.baseurl + PATH + fragmentid + '/children'
+def checknumberofchildren(arguments, base_url, fragmentid):
+    url = base_url + fragmentid + '/children'
     getchildrenrequest = requests.get(url, auth=HTTPBasicAuth(arguments.username, arguments.password))
     jsonresult = getchildrenrequest.json()
     totalnrofresults = jsonresult['totalNrOfResults']
     return totalnrofresults
 
-def performoperations(arguments, fragmentid):
-    url = arguments.baseurl + PATH + fragmentid + '/children'
+def performoperations(arguments, base_url, fragmentid):
+    url = base_url + fragmentid + '/children'
     getchildrenrequest = requests.get(url, auth=HTTPBasicAuth(arguments.username, arguments.password))
     jsonresult = getchildrenrequest.json()
     children = jsonresult['mediaDataList']
@@ -32,30 +32,29 @@ def performoperations(arguments, fragmentid):
     rewritedata = []
     counter = 1
     for child in children:
-        logging.info('\t' + str(counter) + ': ' + child['externalId'])
+        logging.info('\t%s: %s', counter, child['externalId'])
         rewritedata.append(('id', child['fragmentId']))
         counter += 1
 
     randomindex = random.randint(0, len(children) - 1)
 
-    logging.info('Removing item with index ' +
-                 str(randomindex) + ' (' + children[randomindex]['externalId'] +
-                 ') from the ensemble')
+    logging.info('Removing item with index %s (%s) from the ensemble',
+                    randomindex, children[randomindex]['externalId'])
     deletedchild = children[randomindex]['fragmentId']
 
     # Remove the object from the ensemble
-    url = arguments.baseurl + PATH + fragmentid + '/children/' + deletedchild
+    url = base_url + fragmentid + '/children/' + deletedchild
     deletefromensemblerequest = requests.delete(url, auth=HTTPBasicAuth(arguments.username, arguments.password))
 
     # Add object back to ensemble
-    url = arguments.baseurl + PATH + fragmentid + '/children'
+    url = base_url + fragmentid + '/children'
     deletedata = {'id': (None, deletedchild)}
     logging.info('Re-adding the previously deleted object')
     addensemblerequest = requests.post(url, files=deletedata,
                                        auth=HTTPBasicAuth(arguments.username, arguments.password))
 
     # Rewrite ensemble
-    url = arguments.baseurl + PATH + fragmentid + '/children'
+    url = base_url + fragmentid + '/children'
     logging.info('Rewriting the object')
     rewriteensemblerequest = requests.put(url, files=rewritedata,
                                           auth=HTTPBasicAuth(arguments.username, arguments.password))
@@ -64,7 +63,7 @@ def performoperations(arguments, fragmentid):
     logging.info('Sleeping to make sure the update persisted')
     time.sleep(5)
 
-    url = arguments.baseurl + PATH + fragmentid + '/children'
+    url = base_url + fragmentid + '/children'
     getchildrenrequest = requests.get(url, auth=HTTPBasicAuth(arguments.username, arguments.password))
     jsonresult = getchildrenrequest.json()
     totalnrofresults = jsonresult['totalNrOfResults']
@@ -93,51 +92,42 @@ def init_logger():
     except PermissionError:
         logging.error("Permission denied for {}".format(logfile))
 
-def check_arguments(arguments):
-    if arguments.username is None:
-        close("No username specified")
-
-    if arguments.password is None:
-        close("No password specified")
-    if arguments.environment is None:
-        arguments.environment = 'QAS'
-    if arguments.environment == 'QAS':
-        arguments.baseurl = BASEURL_QAS
-    else:
-        arguments.baseurl = BASEURL_PRD
-
 def close(message):
     logging.error(message)
     exit(1)
 
 
 def main():
-    logging.info('Running in environment: ' + arguments.environment)
-
-    getseturl = arguments.baseurl + PATH
+    logging.info('Running in environment: %s', cmd_args.environment)
+    base_url = BASEURL_QAS if cmd_args.environment.lower() == 'qas' else BASEURL_PRD
+    getseturl = base_url + PATH
     # We need to construct the query ourselves (see https://github.com/requests/requests/issues/1454).
     # If we don't, the URL will be: '/mediahaven-rest-api/resources/media/?q=%2B%28MediaObjectType%3ASet%29&nrOfResults=1'
     # Request multiple sets since not all will have children
+    logging.info('Fetching sets from: %s', getseturl)
     getsetrequest = requests.get(getseturl + '?q=%2B(MediaObjectType:Set)',
-                                 auth=HTTPBasicAuth(arguments.username, arguments.password))
+                                 auth=HTTPBasicAuth(cmd_args.username, cmd_args.password))
     getsetresponse = getsetrequest.json();
     nr_of_results = getsetresponse['totalNrOfResults']
-    logging.info('totalNrOfResults is: %s' % nr_of_results)
+    logging.info('Sets found (totalNrOfResults): %s', nr_of_results)
     setresults = getsetresponse['mediaDataList']
     if setresults:
         for complex in setresults:
             fragmentid = complex['fragmentId']
-            logging.debug('Checking fragmentId "%s" for children...' % fragmentid)
-            nrofchildren = checknumberofchildren(arguments, fragmentid)
+            logging.debug('Checking fragmentId "%s" for children...', fragmentid)
+            nrofchildren = checknumberofchildren(cmd_args, getseturl, fragmentid)
             if nrofchildren > 0:
-                logging.info('Found a set (%s) with %s children.' % (fragmentid, nrofchildren))
-                performoperations(arguments, fragmentid)
+                logging.info('Found a set (%s) with %s children.', fragmentid, nrofchildren)
+                performoperations(cmd_args, getseturl, fragmentid)
                 break
 
 if __name__ == "__main__":
-    arguments = Arguments()
     init_logger()
-    parser = argparse.ArgumentParser()
+    # Parse the command line arguments
+    cmd_args = Arguments()
+    parser = argparse.ArgumentParser(prog="ensemble-rewrite-test",
+                description="""Tests the rewrite methods of MediaHaven
+                    for ensembles.""")
     parser.add_argument('-u', '--username', type=str, dest='username',
             required=True,
             help='Username to use when connecting to mediahaven.')
@@ -146,8 +136,6 @@ if __name__ == "__main__":
             help='Password to use when connecting to mediahaven.')
     parser.add_argument('-e', '--environment', type=str, choices=['QAS', 'PRD'],
             help='The environment to test. QAS or PRD.')
-    parser.parse_args(namespace=arguments)
-
-    check_arguments(arguments)
-    
+    parser.parse_args(namespace=cmd_args)
+    # And off we go...
     main()
